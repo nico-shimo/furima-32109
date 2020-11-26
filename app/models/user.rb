f724627ce6c992ea9d5e409e8e23985271438dc2
nico-shimo <kunikuni551981@gmail.com>
2020-11-26 17:13:47 +0900

    新規登録の実装（エラーメッセージ含む）

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birth_day, presence: true
+
+  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # has_many :prototypes, dependent: :destroy
+  has_many :items, dependent: :destroy
+  
+end
