9be8c1e016b93459a1ecd50adef3a6a6ba64f9a5
nico-shimo <kunikuni551981@gmail.com>
2020-11-27 16:30:19 +0900

    ユーザー管理機能（修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     validates_format_of :password, with: PASSWORD_REGEX, message: 'Password    Include both letters and numbers'
     validates :nickname, presence: true
     validates :birth_day, presence: true
-    with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
     validates :first_name
     validates :last_name
     validates :first_name_kana
