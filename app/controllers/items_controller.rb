0f6a45558298d34fe24a1320b2f101acd6a7f282
nico-shimo <kunikuni551981@gmail.com>
2020-11-28 19:23:39 +0900

    商品出品情報を保存するテーブルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,16 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :introduction, :category_id, :condition_id, :postage_id, :shopping_prefecture_id, :days_id, :price, :user)
 end
