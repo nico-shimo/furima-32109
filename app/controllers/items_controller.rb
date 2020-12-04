b4975a97b26c599dc01ff3134add149f2b6b7ec2
nico-shimo <kunikuni551981@gmail.com>
2020-12-04 10:53:01 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
+    if current_user.id == @item.user_id
+      @item.destroy
       redirect_to root_path
+    else
+      render :new
     end
   end
 
